d25b5395088ab49925bc5fe06c3b6154e689d0d5
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-11-03 00:43:48 -0400

    commit tree 4f68547874fd999c170c43ac4c106a9029cde8c2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import { addTodo } from  './actions/todos';
+
 
 class App extends Component {
 
   state = {
     todo: ''
   }
+  addTodo = () => {
+    return ({
+      type: 'ADD_TODO',
+      todo: this.state.todo
+    })
+  }
+  
+
 
   handleOnChange = event => {
     this.setState({
@@ -17,11 +27,11 @@ class App extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     console.log("Todo being added: ", this.state.todo);
-    this.props.dispatch({ type: 'ADD_TODO', todo: this.state.todo });
+    this.props.addTodo(this.state.todo); //Code change: we are no longer calling `dispatch` here
     this.setState({ todo: '' });
   }
-
   render() {
+    // debugger;
     const renderTodos = () => this.props.todos.map(todo => <li key={todo}>{todo}</li>);
     return (
       <div className="App">
@@ -41,10 +51,17 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     todos: state.todos
+//   };
+// };
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addTodo: (todo) => {
+//       dispatch(addTodo(todo))
+//     }
+//   };
+// };
 
-export default connect(mapStateToProps)(App);
+export default connect(state => ({ todos: state.todos }), { addTodo })(App);
